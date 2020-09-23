47a2a8bb987ec86e4cc26a4775653c8d69d60f91
Szymon Graczyk <sz.graczyk@gmail.com>
2020-09-23 23:17:36 +0200

    🎉 Setup project with typescript, eslint, prettier etc.

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+
+import styles from '../../styles/Home.module.css'
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>The Effective Tech Lead</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to{' '}
+          <a href="https://effectivetechlead.com">The Effective Tech Lead!</a>
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
